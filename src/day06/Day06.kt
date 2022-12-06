1aea37cef0c566c0fed8512f8bfba34bd0404ca3
Łukasz Pięta <pieta.lukasz98@gmail.com>
2022-12-06 08:06:28 +0100

    Day 06

---
+++ src/day06/Day06.kt
@@ -0,0 +1,31 @@
+package day06
+
+import java.io.File
+
+fun main() {
+
+    fun part1(input: String): Int {
+        return input.windowedSequence(4, 1)
+            .withIndex()
+            .first {
+                it.value.groupBy { it }.keys.size == 4
+            }
+            .let { it.index + 4 }
+    }
+
+    fun part2(input: String): Int {
+        return input.windowedSequence(14, 1)
+            .withIndex()
+            .first {
+                it.value.groupBy { it }.keys.size == 14
+            }
+            .let { it.index + 14 }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = File("src", "day06/Day06_test.txt").readText()
+
+    val input = File("src", "day06/Day06.txt").readText()
+    println(part1(input))
+    println(part2(input))
+}
