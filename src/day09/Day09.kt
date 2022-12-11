e7f40769778af668fa060a31410b2d80611f5627
Łukasz Pięta <pieta.lukasz98@gmail.com>
2022-12-11 01:15:50 +0100

    Day 09 completed

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -1,30 +1,45 @@
 package day09
 
+import day09.Direction.DOWN
+import day09.Direction.LEFT
+import day09.Direction.RIGHT
+import day09.Direction.UP
 import readInput
 import kotlin.math.abs
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val head = Position(0, 0)
-        val tail = Tail(Position(0, 0))
-        input.map {
-            val (direction, steps) = it.split(" ")
+    fun applyMoves(input: List<String>, knots: List<Knot>) {
+        input.map { line ->
+            val (direction, steps) = line.split(" ")
 
             repeat(steps.toInt()) {
-                val oldHeadPosition = head.copy()
-                head.move(Position.Direction(direction))
-                tail.follow(head, oldHeadPosition)
+                knots.onEach { knot ->
+                    knot.move(Direction(direction))
+                }
             }
         }
+    }
+
+    fun part1(input: List<String>): Int {
+        val head = Knot(id = 0)
+        val tail = Knot(id = 1, following = head)
+
+        applyMoves(input, listOf(head, tail))
 
         return tail.visited.size
     }
 
     fun part2(input: List<String>): Int {
-        //TODO
+        val knots = (1..9)
+            .fold(mutableListOf(Knot(id = 0))) { acc: MutableList<Knot>, index: Int ->
+                acc.add(Knot(index, acc.last()))
+                acc
+            }
 
-        return input.size
+        applyMoves(input, knots)
+
+        return knots.last().visited.size
     }
 
     // test if implementation meets criteria from the description, like:
@@ -35,46 +50,59 @@ fun main() {
     println(part2(input))
 }
 
-class Tail(val position: Position, val visited: MutableSet<Position> = mutableSetOf()) {
+private infix fun Pair<Int, Int>.isExactlyFarInOneAxis(distance: Int): Boolean =
+    (first == 0 && abs(second) == distance) || (abs(first) == distance && second == 0)
+
+data class Knot(
+    val id: Int,
+    val following: Knot? = null,
+    val position: Position = Position(0, 0),
+    val visited: MutableSet<Position> = mutableSetOf()
+) {
 
     init {
         visited.add(position.copy())
     }
 
-    fun follow(head: Position, oldHeadPosition: Position) {
-        val distance = position distanceTo head
+    fun move(direction: Direction) {
+        if (following == null) {
+            position.move(direction)
+            visited.add(position.copy())
+            return
+        }
+
+        val distance = position distanceTo following.position
+        val directions = listOfNotNull(distance.first.getDirection(Axis.X), distance.second.getDirection(Axis.Y))
+
         when {
-            abs(distance.first) == 2 && distance.second == 0 -> {
-                position.moveTo(oldHeadPosition.x, oldHeadPosition.y)
-                visited.add(position.copy())
-            }
-
-            distance.first == 0 && abs(distance.second) == 2 -> {
-                position.moveTo(oldHeadPosition.x, oldHeadPosition.y)
-                visited.add(position.copy())
-            }
-
-            abs(distance.first) + abs(distance.second) == 3 -> {
-                position.moveTo(oldHeadPosition.x, oldHeadPosition.y)
-                visited.add(position.copy())
+            distance isFurtherThanMoves 2 || distance isExactlyFarInOneAxis 2 -> {
+                directions.forEach { position.move(it) }
             }
         }
+        visited.add(position.copy())
     }
 }
 
-data class Position(var x: Int = 0, var y: Int = 0) {
+private infix fun Pair<Int, Int>.isFurtherThanMoves(overallDistance: Int): Boolean =
+    (abs(first) + abs(second)) > overallDistance
 
-    fun moveTo(x: Int, y: Int) {
-        this.x = x
-        this.y = y
+private fun Int.getDirection(axis: Axis): Direction? =
+    when {
+        this > 0 && axis == Axis.X -> RIGHT
+        this < 0 && axis == Axis.X -> LEFT
+        this > 0 && axis == Axis.Y -> UP
+        this < 0 && axis == Axis.Y -> DOWN
+        else -> null
     }
+
+data class Position(var x: Int = 0, var y: Int = 0) {
 
     fun move(direction: Direction): Position {
         when (direction) {
-            Direction.RIGHT -> x += 1
-            Direction.LEFT -> x -= 1
-            Direction.UP -> y += 1
-            Direction.DOWN -> y -= 1
+            RIGHT -> x += 1
+            LEFT -> x -= 1
+            UP -> y += 1
+            DOWN -> y -= 1
         }
         return this
     }
@@ -82,21 +110,26 @@ data class Position(var x: Int = 0, var y: Int = 0) {
     infix fun distanceTo(position: Position): Pair<Int, Int> {
         return Pair(position.x - x, position.y - y)
     }
+}
 
-    enum class Direction {
-        RIGHT,
-        LEFT,
-        UP,
-        DOWN;
+enum class Axis {
+    X,
+    Y
+}
 
-        companion object {
-            operator fun invoke(from: String) = when (from) {
-                "R" -> RIGHT
-                "L" -> LEFT
-                "U" -> UP
-                "D" -> DOWN
-                else -> throw Exception()
-            }
+enum class Direction {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN;
+
+    companion object {
+        operator fun invoke(from: String) = when (from) {
+            "R" -> RIGHT
+            "L" -> LEFT
+            "U" -> UP
+            "D" -> DOWN
+            else -> throw Exception()
         }
     }
 }
